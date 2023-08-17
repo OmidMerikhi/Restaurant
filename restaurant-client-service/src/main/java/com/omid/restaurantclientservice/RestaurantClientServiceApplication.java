8ad8630112de08757ae59c2acd359720877e9474
OmidMerikhi <omidi.merikhi1999@gmail.com>
2023-08-17 15:28:03 +0330

    ok spring-cloud

---
+++ restaurant-client-service/src/main/java/com/omid/restaurantclientservice/RestaurantClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omid.restaurantclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantClientServiceApplication.class, args);
+    }
+
+}
